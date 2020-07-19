dd5ac96b5ce95a3c8e9cf3c26b3bedafad344d58
Lawrence Wagerfield <lawrence@projectone.io>
2020-07-19 13:14:02 +0100

    [WIP] Add predicate model

---
+++ src/main/scala/reverso/FunctionModel.scala
@@ -0,0 +1,52 @@
+package reverso
+
+import cats.effect.{Concurrent, Resource}
+import cats.implicits._
+import fs2.Stream
+import reverso.SolvedValue.SolvedObject
+import reverso.Variables.IntVariable
+import reverso.common.RefPessimistic
+
+/**
+  * Provides access to the function's solutions.
+  *
+  * Thread-safe: yes.
+  */
+class FunctionModel[F[_]: Concurrent](
+  chocoRef: RefPessimistic[F, ChocoState],
+  completedCallStacks: Vector[CallStack],
+  currentCallStack: IntVariable
+) {
+
+  /**
+    * Warning: Deadlock will occur if the thunk passed into 'solutions.use' calls other methods that use [[chocoRef]].
+    *          Please collect the results you need by returning them from the 'solutions.use' thunk, and make subsequent
+    *          calls outside of the thunk if necessary.
+    */
+  def solutions: Resource[F, Stream[F, SolvedObject]] =
+    chocoRef.resourceWithSolver.evalMap {
+      case (choco, solver) =>
+        RefPessimistic.of[F, Unit](()).map { serialConsumer =>
+          Stream
+            .repeatEval {
+              // Even though we're guaranteed no-one else is concurrently using the 'chocoRef' resource, we still cannot
+              // guarantee the consumer of the stream that currently has access to 'chocoRef' is single-threaded. If
+              // it's multi-threaded, the thunk we pass into 'repeatEval' will be executed multiple times in parallel.
+              // Therefore, we wrap the eval in a mutex, ensuring single-threaded access to the model while 'solve' is
+              // executed and snapshots of the variables are taken for the solution.
+              serialConsumer.accessSync { _ =>
+                Option.when(solver.solve()) {
+                  // Todo: Traverse 'pointers' objects below, and construct 'SolvedObject' from it, resolving the
+                  //   *Variables in the 'pointers' objects to *Vars in the 'choco' instance.
+                  val callStack                        = completedCallStacks(choco.ints(currentCallStack).getValue)
+                  val pointers                         = callStack.pointers
+                  val structuredSolution: SolvedObject = ???
+                  structuredSolution
+                }
+              }
+            }
+            .collectWhile { case Some(solution) => solution }
+        }
+    }
+
+}
